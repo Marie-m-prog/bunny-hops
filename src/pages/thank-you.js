28fa63af4e6a42f6d0b964a0d385dd5249ceee1b
marie winther madsen <mariewmadsen@gmail.com>
2020-10-05 13:30:33 +0200

    add form

---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThankYouPage = () => (
+  <Layout>
+    <h1 className='mb-8'>Get in touch</h1>
+    <p>Thank you for your submission!</p>
+    <p className='my-4'><a className='hover:underline' href='/'>Back to homepage</a></p>
+  </Layout>
+)
+
+export default ThankYouPage
